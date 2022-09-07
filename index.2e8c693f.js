6318869583821f698cf1767e5c7e7bda396d02b1
anastacia-titmouse <62905141+anastacia-titmouse@users.noreply.github.com>
2022-09-07 10:00:07 +0000

    Deploying to gh-pages from @ anastacia-titmouse/pinterest-app@08871d58da6c9b1c634da11fce9712d595150a56 🚀

--- index.2e8c693f.js
+++
@@ -1,2 +0,0 @@
-const e=()=>{console.log("cancel complaint")};document.addEventListener("DOMContentLoaded",(()=>{document.getElementById("complaint_modal_cancel").addEventListener("click",e),document.getElementById("desk_selector").addEventListener("change",(e=>{return n=e.target.value,void console.log(n);var n}))}));
-//# sourceMappingURL=index.2e8c693f.js.map
