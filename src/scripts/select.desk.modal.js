dd972353931603f2a16e46f9b463b9c58544e859
Anastacia Protasenya <anastacia.sinitsa@gmail.com>
2022-09-28 18:54:54 +0300

    adds comments to select.desk.modal

--- src/scripts/select.desk.modal.js
+++ src/scripts/select.desk.modal.js
@@ -1,5 +1,6 @@
 import {getDesksModels} from './desk'
 
+//finds pin's id to open modal for exactly this pin, which is clicked on
 export const showSelectBoardModal = (pinId) => {
     const modalWrapper = document.getElementById('select_board_modal')
     modalWrapper.classList.add('visible')
@@ -11,6 +12,7 @@ export const showSelectBoardModal = (pinId) => {
     renderSelectOptions()
 }
 
+//
 export function selectBoardModalFormSubmitHandler(event) {
     event.preventDefault()
 
@@ -20,29 +22,35 @@ export function selectBoardModalFormSubmitHandler(event) {
     const select = document.getElementById("select_board_modal_selector")
     const deskId = select.value
 
+    //finds pin's id in local storage
     const deskPinsJsonData = localStorage.getItem(`desk-pins-${deskId}`)
     let deskPins;
+    //if there are none, create a new set of pins
     if(!deskPinsJsonData) {
         deskPins = new Set()
+    //if there are saved pins in local storage, get the set from the local storage
     } else {
         deskPins = new Set(JSON.parse(deskPinsJsonData))
     }
-
+    //adds pin's id to local storage
     deskPins.add(pinId)
     localStorage.setItem(`desk-pins-${deskId}`, JSON.stringify(Array.from(deskPins)))
     hideSelectBoardModal()
 }
 
+//closes modal
 export function hideSelectBoardModal() {
     const modalWrapper = document.getElementById('select_board_modal')
     modalWrapper.classList.remove('visible')
 }
 
+//cleans select options
 export function cleanSelectOptions() {
     const select = document.getElementById("select_board_modal_selector")
     select.innerHTML = ''
 }
 
+//renders select options
 export function renderSelectOptions() {
     const select = document.getElementById("select_board_modal_selector")
 
